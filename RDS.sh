5daa947bdd91f06b7f04ba8590b5eace79f91c2e
MitPat <mpatange@hawk.iit.edu>
2016-06-27 16:59:08 -0500

    Changes to steps and rds script

--- RDS.sh
+++ RDS.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DBSUBNETNAME=coa-mrp-subnet
+declare -a DBSUBNETNAME=coa-mrp-subnet
 DBINSTANCEIDENTIFIER=coa-mysql-wordpress-db
 DBUSERNAME=controller
 DBPASSWORD=ilovebunnies
@@ -8,28 +8,29 @@ DBNAME=wordpressdb
 SUBNET1=subnet-14fcce3f
 SUBNET2=subnet-1e5e7547
 
-#Step 5a: Create a subnet group
+#Step 1a: Create a subnet group
 
 echo -e "\n Creating subnet group for db instance"
 
 aws rds create-db-subnet-group --db-subnet-group-name $DBSUBNETNAME --db-subnet-group-description MiniProject1 --subnet-ids  $SUBNET1 $SUBNET2
 
-#Step 5b: Launch RDS mysql database
+#Step 1b: Launch RDS mysql database
 
 echo -e "\n Creating db instance"
 aws rds create-db-instance --db-instance-identifier $DBINSTANCEIDENTIFIER --allocated-storage 20 --db-instance-class db.t1.micro --engine mysql --master-username $DBUSERNAME --master-user-password $DBPASSWORD --engine-version 5.6.23 --license-model general-public-license --no-multi-az --storage-type standard --publicly-accessible --availability-zone us-east-1a --db-name $DBNAME --port 3306  --auto-minor-version-upgrade --preferred-maintenance-window mon:00:00-mon:01:30 --vpc-security-group-ids $1 --db-subnet-group-name $DBSUBNETNAME
 
-#Step 5c: Wait for the instance to be available
+#Step 1c: Wait for the instance to be available
 
 echo -e "\n Waiting after launching RDS mysql database to make it available for 10 minutes"
 for i in {0..600}; do echo -ne '.'; sleep 1;done
 
 aws rds wait db-instance-available --db-instance-identifier $DBINSTANCEIDENTIFIER
 
-#Step 6: Describe db instances
+#Step 2: Describe db instances
 
 declare -a DBINSTANCEARR
 
 DBINSTANCEARR=(`aws rds describe-db-instances --output text | grep ENDPOINT | awk {' print $2'}`)
 
 echo ${DBINSTANCEARR[@]}
+
--- Steps-COA.sh
+++ Steps-COA.sh
@@ -12,15 +12,8 @@
 declare -a ELBNAME=coa-mrp-lb
 LAUNCHCONFNAME=coa-mrp-launch-config
 AUTOSCALINGNAME=coa-mrp-asg
-DBSUBNETNAME=coa-mrp-subnet
-DBINSTANCEIDENTIFIER=coa-mysql-wordpress-db
-DBUSERNAME=controller
-DBPASSWORD=ilovebunnies
-DBNAME=wordpressdb
-SUBNET1=subnet-14fcce3f
-SUBNET2=subnet-1e5e7547
 TOPICNAME=coa-notify-mrp
-METRICNAME=coa-cloud-alert-mrp
+METRICNAME=coa-mrp-cloud-alert
 PHONENUMBER=19143193344
 
 #Step 1a: Launch the instances and provide the user-data via the install-env.sh
@@ -73,7 +66,61 @@ aws elb register-instances-with-load-balancer --load-balancer-name $ELBNAME --in
 echo -e "\n Finished launching ELB and registering instances, now sleeping for 25 seconds " 
 for i in {0..25}; do echo -ne '.'; sleep 1;done
 
-echo -e "All Done"
+#Step 3a: Configure launch configuration
+
+echo -e "\n Creating Launch Configuration"
+aws autoscaling create-launch-configuration --launch-configuration-name $LAUNCHCONFNAME --iam-instance-profile $7 --user-data file://../Environment-COA/install-env.sh --key-name $6 --instance-type $3 --security-groups $4 --image-id $1
+
+echo -e "\n Finished launching configuration and sleeping 25 seconds"
+for i in {0..25}; do echo -ne '.'; sleep 1;done
+
+#Step 3b: Configure auto scaling groups
+
+echo -e "\n Creating Auto scaling group"
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name $AUTOSCALINGNAME --launch-configuration-name $LAUNCHCONFNAME --load-balancer-names $ELBNAME --health-check-type ELB --min-size 3 --max-size 6 --desired-capacity 3 --default-cooldown 600 --health-check-grace-period 120 --vpc-zone-identifier $5
+
+echo -e "\n Finished creating auto scaling group and sleeping 25 seconds"
+for i in {0..25}; do echo -ne '.'; sleep 1;done
+
+#Step 4a: Create scale out and scale in policies
+
+declare -a SPURL
+
+SPURL=(`aws autoscaling put-scaling-policy --policy-name coa-mrp-scaleout-policy --auto-scaling-group-name $AUTOSCALINGNAME --scaling-adjustment 3 --adjustment-type ChangeInCapacity`)
+
+echo -e "\n The Scale out policy ARN " ${SPURL[@]}
+
+declare -a SPURL1
+
+SPURL1=(`aws autoscaling put-scaling-policy --policy-name coa-mrp-scalein-policy --auto-scaling-group-name $AUTOSCALINGNAME --scaling-adjustment -3 --adjustment-type ChangeInCapacity`)
+
+echo -e "\n The Scale in policy ARN " ${SPURL1[@]}
+
+#Step 4b: Create an SNS topic for cloud watch metrics subscriptions
+
+METRICARN=(`aws sns create-topic --name $METRICNAME`)
+aws sns set-topic-attributes --topic-arn $METRICARN --attribute-name DisplayName --attribute-value $METRICNAME
+
+#Step 4c: Subscribe user phone number to the topic
+
+aws sns subscribe --topic-arn $METRICARN --protocol sms --notification-endpoint $PHONENUMBER
+
+#Step 4d: Launch cloud metrics for the auto scaling group and sns topic
+
+aws cloudwatch put-metric-alarm --alarm-name AddCapacity --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 30 --comparison-operator GreaterThanOrEqualToThreshold --dimensions "Name=AutoScalingGroupName,Value=$AUTOSCALINGNAME" --evaluation-periods 1 --unit Percent --alarm-actions ${SPURL[@]} ${METRICARN[@]}
+
+aws cloudwatch put-metric-alarm --alarm-name RemoveCapacity --metric-name CPUUtilization --namespace AWS/EC2 --statistic Average --period 60 --threshold 10 --comparison-operator LessThanOrEqualToThreshold --dimensions "Name=AutoScalingGroupName,Value=$AUTOSCALINGNAME" --evaluation-periods 1 --unit Percent --alarm-actions ${SPURL1[@]} ${METRICARN[@]}
+
+echo -e "\n Finished creating cloud watch metrics and sleeping 25 seconds"
+for i in {0..25}; do echo -ne '.'; sleep 1;done
+
+
+#Step 5: Launch ELB in firefox
+
+echo -e "\n Waiting an additional 1 minute before opening the ELB in browser"
+for i in {0..60}; do echo -ne '.'; sleep 1;done
+
+firefox $ELBURL &
 
 
 
